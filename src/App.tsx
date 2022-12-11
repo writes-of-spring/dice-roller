e05430c07ada43ec9af028295fba423cf767fd33
Peter Kerins <pete.kerins@gmail.com>
2022-12-11 11:41:46 +0000

    use refs for form inputs
    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,48 @@
 import React, { useState } from "react";
 
 function App() {
-  const [rolledDice, setRolledDice] = useState([1]);
-  const [numberOfDice, setNumberOfDice] = useState(2);
-  const [typeOfDice, setTypeofDice] = useState(6);
+  const [dicePool, setDicePool] = useState<number[] | null>(null);
+  const numberOfDiceRef = React.useRef<HTMLInputElement>(null);
+  const typeOfDiceRef = React.useRef<HTMLSelectElement>(null);
 
   function rollDice() {
+    if (!numberOfDiceRef.current?.value || !typeOfDiceRef.current?.value) return;
+    const numberOfDice = parseInt(numberOfDiceRef.current?.value || "2");
+    const typeOfDice = parseInt(typeOfDiceRef.current?.value || "6");
     const rolledDice = Array.from({ length: numberOfDice }, () => Math.floor(Math.random() * typeOfDice + 1));
-    setRolledDice(rolledDice);
+    setDicePool(rolledDice);
   }
-  const diceResult = rolledDice.reduce((acc, dice) => {
-    return acc + dice;
-  }, 0);
+  const diceResult =
+    dicePool?.reduce((acc, dice) => {
+      return acc + dice;
+    }, 0) ?? null;
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     rollDice();
   }
+  const showIndividualDice = dicePool && dicePool.length > 1;
   return (
     <div className="md:mt-4 rounded-xl p-4 container mx-auto bg-white text-gray-700">
-      <h1 className="text-5xl text-center text-brand">Pete's Dice Roller</h1>
-      <div className="flex">
-        <div>
+      <h1 className="text-5xl text-center font-bold text-gray-900  sm:mb-4">Pete's Dice Roller</h1>
+      <div className="flex gap-4 flex-wrap-reverse">
+        <div className="w-72 grow ">
           <form onSubmit={handleSubmit}>
             <label className="block">
               <span className="text-gray-700">Number of dice</span>
               <input
-                className="form-input mt-1 block w-full"
-                placeholder="2"
+                name="numberOfDice"
+                ref={numberOfDiceRef}
+                className="form-input mt-1 block "
+                defaultValue={2}
                 min={1}
                 max={10}
-                value={numberOfDice}
                 type="number"
-                onChange={(e) => setNumberOfDice(parseInt(e.target.value))}
               />
             </label>
             <label className="block">
               <span className="text-gray-700">Type of dice</span>
-              <select
-                className="form-input mt-1 block w-full"
-                placeholder="6"
-                value={typeOfDice}
-                onChange={(e) => setTypeofDice(parseInt(e.target.value))}
-              >
+              <select name="typeOfDice" ref={typeOfDiceRef} className="form-input mt-1 block " placeholder="6">
                 {" "}
                 <option value="4">D4</option>
                 <option value="6">D6</option>
@@ -55,12 +55,18 @@ function App() {
             <button>Roll</button>
           </form>
         </div>
-        <div>
-          <h2>
-            Rolling {numberOfDice} D{typeOfDice}
-          </h2>
-          <p>Result: {diceResult}</p>
-          <p>Individual dice: {rolledDice.join(", ")}</p>
+        <div className="w-72 grow">
+          {dicePool ? (
+            <>
+              <h2 className="text-4xl text-gray-700">Result: {diceResult}</h2>
+              <p className="text-2xl text-gray-700">
+                Rolling {numberOfDiceRef.current?.value} D{typeOfDiceRef.current?.value}
+              </p>
+              {showIndividualDice && <p className="text-2xl text-gray-600">Individual dice: {dicePool.join(", ")}</p>}
+            </>
+          ) : (
+            <h2 className="text-4xl text-gray-700">Your adventure awaits!</h2>
+          )}
         </div>
       </div>
     </div>
